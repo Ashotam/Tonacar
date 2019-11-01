e144de4ef6dd93f66feacd66687233bb10284b3b
Ashotam <amiraghyan.ashot@gmail.com>
2019-11-01 19:10:45 +0400

    commit

--- index.js
+++ index.js
@@ -2,16 +2,21 @@
 let x= 0;
 let Changer = () =>{
     let list = document.getElementsByClassName("oneDiv");
+    let  container = document.getElementsByClassName("redContainer");
     if(x === 0){
         for(let i =0;i<list.length;i++){
             list[i].style.display = "flex"
-        }
 
+        }
+        console.log(container);
+        container[0].style.paddingBottom = "50px";
         x ++
     }
     else {
         for(let i =0;i<list.length;i++){
-        list[i].style.display = "none"
+        list[i].style.display = "none";
+            container[0].style.paddingBottom = "0px";
+
     }
         x = 0;
     }
